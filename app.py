54a7d203b0d5f4775bf3a2fc3925100d5f0ff9b0
HD599 <csansamak@gmail.com>
2025-10-06 17:38:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -7,6 +6,4 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+# ไม่ต้องมี app.run() ถ้าใช้ Gunicorn
